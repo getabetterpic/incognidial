93969a70420a8694a4d196e07267071f5153f412
Dan Smith <dan@getabetterpic.com>
2024-12-14 03:30:31 -0500

    Initial commit

---
+++ apps/incognidial-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+export default {
+  displayName: 'incognidial-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/incognidial-e2e',
+};
