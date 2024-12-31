a87fde7f87eeaf7de82075ab7321a34398179f6b
Dan Smith <dan@getabetterpic.com>
2024-12-31 04:31:35 -0500

    Add home and register components

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./users/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'register',
+    pathMatch: 'full',
+  },
+];
