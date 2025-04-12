223b1e58d71efc3f225bda0686f0e4b6a70a97a8
Dan Smith <dan@getabetterpic.com>
2025-04-12 04:11:54 -0400

    Add initial registration form

--- apps/frontend/src/app/users/register.component.ts
+++ apps/frontend/src/app/users/register.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'dial-register',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm = new FormBuilder().group({
+    phoneNumber: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(12)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(12)]],
+  });
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+}
